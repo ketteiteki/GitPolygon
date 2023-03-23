88abee0988e0e53ebe2fe18e4fc95d7afb82447e
kami no me <kudryashov.kd@gmail.com>
2023-03-23 07:36:22 +0400

    3

--- GitPolygon/Controllers/WeatherForecastController.cs
+++ GitPolygon/Controllers/WeatherForecastController.cs
@@ -8,8 +8,10 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "221", "4343"
     };
+
+    private static readonly int Code = 12342;
 
     private readonly ILogger<WeatherForecastController> _logger;
 
@@ -22,6 +24,7 @@ public class WeatherForecastController : ControllerBase
     public IEnumerable<WeatherForecast> Get()
     {
         Console.WriteLine(1);
+        Console.WriteLine(2);
         Console.WriteLine(2);
     
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
