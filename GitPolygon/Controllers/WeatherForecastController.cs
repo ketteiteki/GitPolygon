370b585a327237d4a54fc5b310c2d802619ce919
kami no me <kudryashov.kd@gmail.com>
2023-04-09 17:36:58 +0400

    sq 3

--- GitPolygon/Controllers/WeatherForecastController.cs
+++ GitPolygon/Controllers/WeatherForecastController.cs
@@ -22,8 +22,12 @@ public class WeatherForecastController : ControllerBase
     public IEnumerable<WeatherForecast> Get()
     {
         Console.WriteLine(1);
-        Console.WriteLine(1); // commit 4
-        Console.WriteLine(1); // commit 5
+        Console.WriteLine(2);
+        Console.WriteLine(3);
+        Console.WriteLine(4);
+        Console.WriteLine(5);
+        Console.WriteLine(6);
+        Console.WriteLine(7);
     
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
--- GitPolygon/Program.cs
+++ GitPolygon/Program.cs
@@ -1,15 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,3 +20,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
