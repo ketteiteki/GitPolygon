d91da7788be982e2cae9acabcaa9184ded22684d
kami no me <kudryashov.kd@gmail.com>
2023-04-15 00:52:31 +0400

    commit 1

--- GitPolygon/Controllers/WeatherForecastController.cs
+++ GitPolygon/Controllers/WeatherForecastController.cs
@@ -22,8 +22,13 @@ public class WeatherForecastController : ControllerBase
     public IEnumerable<WeatherForecast> Get()
     {
         Console.WriteLine(1);
-        Console.WriteLine(1); // commit 4
-        Console.WriteLine(1); // commit 5
+        Console.WriteLine(2);
+        Console.WriteLine(3);
+        Console.WriteLine(4);
+        Console.WriteLine(5);
+        Console.WriteLine(6);
+        Console.WriteLine(7);
+        Console.WriteLine(8);
     
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
