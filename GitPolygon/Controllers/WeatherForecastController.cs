2241f2a19727f315ee6b11d2fd7c7d011dc44568
Ketteiteki <kudryashov.kd@gmail.com>
2023-08-20 08:50:35 +0400

    Revert "commit 3"

--- GitPolygon/Controllers/WeatherForecastController.cs
+++ GitPolygon/Controllers/WeatherForecastController.cs
@@ -21,7 +21,6 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        Console.WriteLine(3); // commit 3
         Console.WriteLine(4); // commit 4
         
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
